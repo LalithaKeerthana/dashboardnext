2c273cc2a08936b19fa8d3b1fc918ad72999831e
Lalitha Keerthana <lalithakambampati098@gmail.com>
2024-02-25 11:52:13 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,56 +10,51 @@ import SalesCard, { SalesProps } from "@/components/SalesCard";
 const cardData: CardProps[] = [
   {
     label: "Total Revenue",
-    amount: "$45,231.89",
-    discription: "+20.1% from last month",
+    amount: "45k",
+    discription: "",
     icon: DollarSign
   },
   {
     label: "Subscriptions",
-    amount: "+2350",
-    discription: "+180.1% from last month",
+    amount: "2k",
+    discription: "",
     icon: Users
   },
   {
     label: "Sales",
-    amount: "+12,234",
-    discription: "+19% from last month",
+    amount: "12k",
+    discription: "",
     icon: CreditCard
   },
   {
     label: "Active Now",
-    amount: "+573",
-    discription: "+201 since last hour",
+    amount: "5k",
+    discription: "",
     icon: Activity
   }
 ];
 
 const uesrSalesData: SalesProps[] = [
   {
-    name: "Olivia Martin",
-    email: "olivia.martin@email.com",
-    saleAmount: "+$1,999.00"
+    name: "user1",
+    email: "",
+    saleAmount: "2000"
   },
   {
-    name: "Jackson Lee",
-    email: "isabella.nguyen@email.com",
-    saleAmount: "+$1,999.00"
+    name: "user1",
+    email: "",
+    saleAmount: "2000"
   },
   {
-    name: "Isabella Nguyen",
-    email: "isabella.nguyen@email.com",
-    saleAmount: "+$39.00"
+    name: "user1",
+    email: "",
+    saleAmount: "2000"
   },
-  {
-    name: "William Kim",
-    email: "will@email.com",
-    saleAmount: "+$299.00"
+  {name: "user1",
+  email: "",
+  saleAmount: "2000"
   },
-  {
-    name: "Sofia Davis",
-    email: "sofia.davis@email.com",
-    saleAmount: "+$39.00"
-  }
+  
 ];
 
 export default function Home() {
@@ -87,7 +82,7 @@ export default function Home() {
           <section>
             <p>Recent Sales</p>
             <p className="text-sm text-gray-400">
-              You made 265 sales this month.
+               this month
             </p>
           </section>
           {uesrSalesData.map((d, i) => (
